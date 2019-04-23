4a7d9447fa6875d58863d4998ff07efb4e1fd439
6720601 <46965875+6720601@users.noreply.github.com>
2019-04-23 08:38:52 -0500

    Added Database

--- CrimeLab.java
+++ CrimeLab.java
@@ -1,6 +1,9 @@
 package com.ctech.ingalls.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.ctech.ingalls.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -21,6 +26,9 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         }
 
 
